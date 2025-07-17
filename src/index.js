bcc124a9282c575bd72b1cd95c293d12ca0714fe
Tarek nageib <tarek.nageib303@gmail.com>
2025-07-17 18:13:16 +0300

    edit some code

--- src/index.js
+++ src/index.js
@@ -4,22 +4,16 @@ import { Smoothscrolling } from "./utils/smoothscrolling.js"
 window.Webflow ||= [];
 window.Webflow.push(() => {
     document.addEventListener("DOMContentLoaded", () => {
-        // ✅ Register all needed GSAP plugins once
-        gsap.registerPlugin(
-            ScrollTrigger,
-            ScrollToPlugin,
-            GSDevTools,
-            Flip,
-            ScrollSmoother,
-            SplitText
-        );
+
         // ✅ Register smooth scroll
         Smoothscrolling()
+
+
+
         // ✅ gsap aniamtions
         Home();
 
-        // ✅ Debug panel (remove in production)
-        GSDevTools.create({ animation: master, id: "master" });
+
     })
 
 });
