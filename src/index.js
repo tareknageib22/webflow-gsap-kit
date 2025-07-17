74455e9dda703c7588f9587c222b756263c5bdf1
Tarek nageib <tarek.nageib303@gmail.com>
2025-07-17 19:05:46 +0300

    9

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import { Home } from "./pages/home/home.js"
-import { Smoothscrolling } from "./utils/smoothscrolling.js"
+// import { Home } from "./pages/home/home.js"
+// import { Smoothscrolling } from "./utils/smoothscrolling.js"
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
     document.addEventListener("DOMContentLoaded", () => {
-
-        // ✅ Register smooth scroll
-        Smoothscrolling()
-
-
-
-        // ✅ gsap aniamtions
-        Home();
-
+console.log("hi")
 
     })
 
